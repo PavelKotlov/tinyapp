5c43cd41e893c7dcc1747040d13874f7fe10d79b
PavelKotlov <pavelkotlov@gmail.com>
2023-02-20 19:39:24 -0800

    Refactored the server file to remove the route handler examples from the beginning of the exercise

--- express_server.js
+++ express_server.js
@@ -28,18 +28,6 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
