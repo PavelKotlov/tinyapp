efc65fdada300e2e1bff3a8321469de0a7d7fedd
PavelKotlov <pavelkotlov@gmail.com>
2023-02-23 20:05:33 -0800

    Compared to make sure all endpoints match project requirement checklist, and added password hashing upon registration and password comparison upon login

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const cookieParser = require('cookie-parser');
 const morgan = require("morgan");
 const app = express();
@@ -17,9 +18,7 @@ app.use(cookieParser());
 
 // App data
 const usersDatabase = {};
-
-const urlDatabase = {
-};
+const urlDatabase = {};
 
 
 // Global Variables
@@ -41,7 +40,7 @@ const getUserByEmail = (email) => {
   return null;
 };
 
-const getURLById = (urlID) => {
+const confirmURLById = (urlID) => {
   for (const URL in urlDatabase) {
     if (URL === urlID) {
       return true;
@@ -51,36 +50,104 @@ const getURLById = (urlID) => {
   return null;
 };
 
+const urlsForUser = (user_id) => {
+  const userURLs = {};
+  for (const url in urlDatabase) {
+    if (urlDatabase[url].userID === user_id) {
+      userURLs[url] = urlDatabase[url];
+    }
+  }
+
+  return userURLs;
+};
+
 
 // Route handlers
 // GET Requests #################################
 
+// GET /
+app.get("/", (req, res) => {
+  const user_id = req.cookies["user_id"];
+  if (user_id) {
+    return res.redirect("/urls");
+  }
+
+  res.redirect("/login");
+});
 // GET /urls
 app.get("/urls", (req, res) => {
   const user_id = req.cookies["user_id"];
-  
+
+  if(!user_id) {
+    const errorParameters = {
+      user: usersDatabase[user_id],
+      code: 401,
+      message: "Unautorized access. Please login or register to access."
+    }
+    res.statusCode = 401;
+    res.render("error_page", errorParameters);
+    return;
+  }
+
   const templateVars = {
     user: usersDatabase[user_id],
-    urls: urlDatabase
+    urls: urlsForUser(user_id)
   };
 
   res.render("urls_index", templateVars);
+
 });
 // GET /urls/new
 app.get("/urls/new", (req, res) => {
   const user_id = req.cookies["user_id"];
   if (!user_id) {
-    res.redirect("/login");
-    return;
+    return res.redirect("/login");
   }
+
   res.render("urls_new", { user: usersDatabase[user_id] });
 });
 // GET /urls/:id
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const user_id = req.cookies["user_id"];
+
+  if (!user_id) {
+    const errorParameters = {
+      user: usersDatabase[user_id],
+      code: 401,
+      message: "Unautorized access. Please login or register to access."
+    }
+    res.statusCode = 401;
+    res.render("error_page", errorParameters);
+    return;
+  }
   
-  if (!getURLById(id)) {
+  if (!confirmURLById(id) || !urlsForUser(user_id)[id]) {
+    const errorParameters = {
+      user: usersDatabase[user_id],
+      code: 400,
+      message: "URL does not exist, please create new short URL."
+    }
+    res.statusCode = 400;
+    res.render("error_page", errorParameters);
+    return;
+  }
+  
+  
+  const templateVars = {
+    user: usersDatabase[user_id],
+    id: id,
+    longURL: urlsForUser(user_id)[id].longURL
+  };
+  
+  res.render("urls_show", templateVars);
+});
+// GET /u/:id
+app.get("/u/:id", (req, res) => {
+  const id = req.params.id;
+  const user_id = req.cookies["user_id"];
+
+  if (!confirmURLById(id)) {
     const errorParameters = {
       user: usersDatabase[user_id],
       code: 400,
@@ -91,32 +158,7 @@ app.get("/urls/:id", (req, res) => {
     return;
   }
 
-  const templateVars = {
-    user: usersDatabase[user_id],
-    id: id,
-    longURL: urlDatabase[id].longURL
-  };
-  
-  res.render("urls_show", templateVars);
-});
-// GET /u/:id
-app.get("/u/:id", (req, res) => {
-  const id = req.params.id;
-  const longURL = urlDatabase[id].longURL;
-  const user_id = req.cookies["user_id"];
-  
-  if (!getURLById(id)) {
-    const errorParameters = {
-      user: usersDatabase[user_id],
-      code: 302,
-      message: "Invalid short URL id."
-    }
-    res.statusCode = 302;
-    res.render("error_page", errorParameters);
-    return;
-  }
-
-  res.redirect(longURL);
+  res.redirect(urlDatabase[id].longURL);
 });
 // GET /login
 app.get("/login", (req, res) => {
@@ -145,45 +187,80 @@ app.post("/urls", (req, res) => {
   if (!user_id) {
     const errorParameters = {
       user: usersDatabase[user_id],
-      code: 403,
+      code: 401,
       message: "Please log in to create new short urls."
     }
-    res.statusCode = 403;
+    res.statusCode = 401;
     res.render("error_page", errorParameters);
     return;
   }
 
   const randomId = generateRandomString(6);
-  if (req.body.longURL) {
-    urlDatabase[randomId] = {
-      longURL: req.body.longURL,
-      userID: user_id
-    }
-    res.redirect(`/urls/${randomId}`);
+  urlDatabase[randomId] = {
+    longURL: req.body.longURL,
+    userID: user_id
   }
-  console.log(urlDatabase);
+
+  res.redirect(`/urls/${randomId}`);
 });
 // POST /urls/:id
 app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
   const user_id = req.cookies["user_id"];
+  
+  if (!user_id) {
+    const errorParameters = {
+      user: usersDatabase[user_id],
+      code: 401,
+      message: "Please log in to edit exisitng urls."
+    }
+    res.statusCode = 401;
+    res.render("error_page", errorParameters);
+    return;
+  }
 
-  const templateVars = {
-    user: usersDatabase[user_id],
-    id: id,
-    longURL: urlDatabase[id].longURL
-  };
+  if (!confirmURLById(id)) {
+    const errorParameters = {
+      user: usersDatabase[user_id],
+      code: 400,
+      message: "Invalid short URL id."
+    }
+    res.statusCode = 400;
+    res.render("error_page", errorParameters);
+    return;
+  }
 
-  res.render("urls_show", templateVars);
-});
-// POST /urls/:id/edit
-app.post("/urls/:id/edit", (req, res) => {
-  urlDatabase[req.body.id].longURL = req.body.longURL;
-  res.redirect(`/urls`);
+  urlDatabase[id].longURL = req.body.longURL;
+
+  res.redirect("/urls");
 });
 // POST /urls/:id/delete
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
+  const user_id = req.cookies["user_id"];
+  
+  if (!user_id) {
+    const errorParameters = {
+      user: usersDatabase[user_id],
+      code: 401,
+      message: "Please log in to delete existing urls."
+    }
+    res.statusCode = 401;
+    res.render("error_page", errorParameters);
+    return;
+  }
+
+  if (!urlsForUser(user_id)[id]) {
+    const errorParameters = {
+      user: usersDatabase[user_id],
+      code: 400,
+      message: "Invalid short URL id."
+    }
+    res.statusCode = 400;
+    res.render("error_page", errorParameters);
+    return;
+  }
+
   delete urlDatabase[id];
   res.redirect("/urls");
 });
@@ -204,20 +281,23 @@ app.post("/login", (req, res) => {
     res.render("error_page", errorParameters);
     return;
   }
-
-  if (userFound.password !== userLoginPassword) {
-    const errorParameters = {
-      user: usersDatabase[user_id],
-      code: 403,
-      message: "Invalid password. Please check your password details and try again."
+  
+  bcrypt.compare(userLoginPassword, userFound.password)
+  .then(function(result) {
+    if (!result) {
+      const errorParameters = {
+        user: usersDatabase[user_id],
+        code: 403,
+        message: "Invalid password. Please check your password details and try again."
+      }
+      res.statusCode = 403;
+      res.render("error_page", errorParameters);
+      return;
     }
-    res.statusCode = 403;
-    res.render("error_page", errorParameters);
-    return;
-  }
 
-  res.cookie("user_id", userFound.id);
-  res.redirect("/urls");
+    res.cookie("user_id", userFound.id);
+    res.redirect("/urls");
+  });
 });
 // POST /logout
 app.post("/logout", (req, res) => {
@@ -252,16 +332,23 @@ app.post("/register", (req, res) => {
     return;
   }
 
-  const userId = generateRandomString(8);
-  const userParameters = {
-    id: userId,
-    email: newUserEmail,
-    password: newUserPassword
-  };
-  usersDatabase[userId] = userParameters;
+  bcrypt.genSalt(10)
+  .then((salt) => {
+    return bcrypt.hash(newUserPassword, salt);
+  })
+  .then((hash) => {
+    const userId = generateRandomString(8)
+    const userParameters = {
+      id: userId,
+      email: newUserEmail,
+      password: hash
+    };
 
-  res.cookie("user_id", userId);
-  res.redirect("/urls");
+    usersDatabase[userId] = userParameters;
+
+    res.cookie("user_id", userId);
+    res.redirect("/urls");
+  });
 });
 
 
